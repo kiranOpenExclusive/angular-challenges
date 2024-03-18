67d4272e53e40e461ccaac0b3adddee66ed59d55
Kiran R <kiran.r@opn-mc-357.wwo.local>
2024-03-18 16:40:36 +0530

    feat: implemented routing in angular with smooth scroll

--- apps/angular/anchor-scrolling/src/app/nav-button.component.ts
+++ apps/angular/anchor-scrolling/src/app/nav-button.component.ts
@@ -1,10 +1,21 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+interface targetElementInterface {
+  [key: string]: string;
+}
+
+const TARGET_ELEMENT_ID: targetElementInterface = {
+  '#bottom': 'bottom',
+  '#top': 'top',
+};
 @Component({
   selector: 'nav-button',
   standalone: true,
+  imports: [RouterModule],
   template: `
-    <a [href]="href">
+    <a [routerLink]="href" (click)="handleAnchorTagClick()">
       <ng-content></ng-content>
     </a>
   `,
@@ -14,4 +25,30 @@ import { Component, Input } from '@angular/core';
 })
 export class NavButtonComponent {
   @Input() href = '';
+  @ViewChild('top') topElement!: ElementRef;
+  @ViewChild('bottom') bottomElement!: ElementRef;
+
+  scrollToTarget(element: HTMLElement | null): void {
+    if (!element) {
+      return;
+    }
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  handleAnchorTagClick(): void {
+    if (!this.href.startsWith('#')) {
+      return;
+    }
+
+    switch (this.href) {
+      case '#bottom':
+        this.scrollToTarget(document.getElementById('bottom'));
+        return;
+      case '#top':
+        this.scrollToTarget(document.getElementById('top'));
+        return;
+      default:
+        return;
+    }
+  }
 }
