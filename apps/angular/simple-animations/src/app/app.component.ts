7138931eeb1fa5c8e4bc1655539684906c591cc1
Kiran R <kiran.r@opn-mc-357.wwo.local>
2024-03-19 14:59:21 +0530

    feat: implemented fade-in and staggered animation

--- apps/angular/simple-animations/src/app/app.component.ts
+++ apps/angular/simple-animations/src/app/app.component.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  keyframes,
+  query,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   standalone: true,
   imports: [],
   selector: 'app-root',
+  animations: [
+    trigger('animateLeftPara', [
+      state(
+        'start',
+        style({
+          opacity: 0,
+          transform: 'translateX(-500px)',
+        }),
+      ),
+      state(
+        'stop',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        }),
+      ),
+      transition('start => stop', [animate('0.3s')]),
+    ]),
+    trigger('staggerAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(
+          ':enter',
+          stagger('100ms', [
+            animate(
+              '300ms 200ms',
+              keyframes([
+                style({
+                  opacity: 0,
+                  transform: 'translateX(-20px)',
+                  offset: 0,
+                }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateX(10px)',
+                  offset: 0.3,
+                }),
+                style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+              ]),
+            ),
+          ]),
+        ),
+      ]),
+    ]),
+  ],
   styles: `
     section {
       @apply flex flex-1 flex-col gap-5;
@@ -19,7 +74,7 @@ import { Component } from '@angular/core';
   `,
   template: `
     <div class="mx-20 my-40 flex gap-5">
-      <section>
+      <section [@animateLeftPara]="startAnimation ? 'start' : 'stop'">
         <div>
           <h3>2008</h3>
           <p>
@@ -51,38 +106,30 @@ import { Component } from '@angular/core';
         </div>
       </section>
 
-      <section>
-        <div class="list-item">
-          <span>Name:</span>
-          <span>Samuel</span>
-        </div>
-
-        <div class="list-item">
-          <span>Age:</span>
-          <span>28</span>
-        </div>
-
-        <div class="list-item">
-          <span>Birthdate:</span>
-          <span>02.11.1995</span>
-        </div>
-
-        <div class="list-item">
-          <span>City:</span>
-          <span>Berlin</span>
-        </div>
-
-        <div class="list-item">
-          <span>Language:</span>
-          <span>English</span>
-        </div>
-
-        <div class="list-item">
-          <span>Like Pizza:</span>
-          <span>Hell yeah</span>
-        </div>
+      <section @staggerAnimation>
+        @for (item of list; track item.key) {
+          <div class="list-item">
+            <span>{{ item.key }}:</span>
+            <span>{{ item.value }}</span>
+          </div>
+        }
       </section>
     </div>
   `,
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  startAnimation = true;
+
+  readonly list: { key: string; value: string }[] = [
+    { key: 'Name', value: 'Samuel' },
+    { key: 'Age', value: '28' },
+    { key: 'Birthdate', value: 'City' },
+    { key: 'Language', value: 'English' },
+    { key: 'Like Pizza', value: 'Hell yeah' },
+  ];
+
+  constructor() {}
+  ngAfterViewInit(): void {
+    this.startAnimation = false;
+  }
+}
