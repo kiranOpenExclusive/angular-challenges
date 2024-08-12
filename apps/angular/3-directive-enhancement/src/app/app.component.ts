8559a512440f743a2007a8dd17b8e4444e6e5394
kiranOpenExclusive <163113836+kiranOpenExclusive@users.noreply.github.com>
2024-08-12 17:10:50 +0530

    feat: directive enhancement addded for ngFor with conditional rendering of TemplateRef

--- apps/angular/3-directive-enhancement/src/app/app.component.ts
+++ apps/angular/3-directive-enhancement/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgForEmpty } from './ngForEmpty.directive';
 
 interface Person {
   name: string;
@@ -7,19 +8,24 @@ interface Person {
 
 @Component({
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgForEmpty],
   selector: 'app-root',
   template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
+    <div *ngFor="let person of persons; empty: emptyList">
+      {{ person.name }}
+    </div>
+
     <ng-template #emptyList>The list is empty !!</ng-template>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  persons: Person[] = [];
+  persons: Person[] = [
+    { name: 'test1' },
+    { name: 'test2' },
+    { name: 'test3' },
+    { name: 'test4' },
+    { name: 'test5' },
+  ];
 }
